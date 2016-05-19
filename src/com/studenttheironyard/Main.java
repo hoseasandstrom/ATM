450ade101f9769e3bab71f686f6e0f73a37d2f52
hoseasandstrom <Lakyn4ever>
2016-05-19 10:04:18 -0400

    update

--- src/com/studenttheironyard/Main.java
+++ src/com/studenttheironyard/Main.java
@@ -4,35 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static Scanner scanner;
+
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome!");
         System.out.println("What's your name?");
 
         Scanner scanner = new Scanner(System.in);
 
-        String name = scanner.nextLine();
-        System.out.println("Welcome " + name);
+        Customer customer = new Customer();
 
-        System.out.println("Choose your option: [Check my balance, Withdraw funds, or Cancel transaction]");
-        String option = scanner.nextLine();
+        customer.chooseName();
+        customer.chooseOption();
 
-        if (option.equalsIgnoreCase("Check my balance")) {
-            System.out.println("Your balance is $100.00");
-
-        } else if (option.equalsIgnoreCase("Cancel")) {
-            System.out.println("Thank you, please come again!");
-
-        } else if (option.equalsIgnoreCase("Withdraw funds")) {
-            System.out.println("Enter the amount you wish to withdraw");
-            String num = scanner.nextLine();
-            int numInt = Integer.valueOf(num);
-
-            if (numInt < 100) {
-                System.out.println("Please take you money and have a nice day!");
-            } else {
-                throw new Exception("You don't have money like that!");
-
-            }
-        }
     }
 }
